4d7e97cf5965b1968213f8ffc52b4a7da13aae70
sridhar-reddy <sridharreddy.93953@gmail.com>
2020-09-11 20:41:49 +0530

    1-color-flipper project completed.

--- 1-color-flipper/src/app.js
+++ 1-color-flipper/src/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const button = document.getElementById("btn");
+const colorSpan = document.querySelector(".color");
+
+button.addEventListener("click", changeBackgroundColor);
+
+
+function changeBackgroundColor() {
+    const randomNumber = generateRandom();
+    document.body.style.backgroundColor = colors[randomNumber];
+    colorSpan.textContent = colors[randomNumber];
+}
+
+function generateRandom() {
+    const generatedNumber = Math.floor(Math.random() * colors.length);
+    return generatedNumber;
+}
--- 1-color-flipper/src/hex.js
+++ 1-color-flipper/src/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const button = document.getElementById("btn");
+const colorSpan = document.querySelector(".color");
+
+button.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+    let hexColor = "#";
+    for(let i = 0; i < 6; i++){
+        hexColor += hex[randomNumber()];
+    }
+
+    document.body.style.backgroundColor = hexColor;
+    colorSpan.textContent = hexColor;
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
